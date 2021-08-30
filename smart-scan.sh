41654ba1d199b1a98dd8c004671494039de7f9dc
uchiafarmer <uchiafarmer@gmail.com>
2021-08-30 13:03:56 +0100

    Added support for more than 24 drives

--- smart-scan.sh
+++ smart-scan.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# Runs a smart-health scan of attached storage
 
 if ! [[ $EUID = 0 ]]; then
     echo "Please run this script with 'sudo'..."
     exit 1
 fi
 
-for drive in $(ls /dev/sd[a-z][1-9])
+for drive in $(ls /dev/sd*[1-9])
 do
     echo -en "$drive \t"
     smartctl -H $drive | grep "Health Status"
